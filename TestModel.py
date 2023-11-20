2e07fb24656b7067d6e534ef29052a6b2cf8b1a0
berkbig <berkbig@hotmail.com>
2023-11-20 16:14:10 +0000

    First working Model

--- TestModel.py
+++ TestModel.py
@@ -2,10 +2,67 @@
 from build123d import *
 from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
 set_port(3939)
-
 #%%
-with BuildPart() as test:
-    Box(3,3,3)
+def svgtosketch(svg_file):
+    fp = import_svg(svg_file)
+    fp=list(fp)
+    with BuildSketch() as sk:
+        for f in fp:
+            add(f)
+    return sk.sketch
 
-show_object(test)
+def StringToPart(textstring,font="Copperplate Gothic Bold",font_size=11,align=(Align.CENTER, Align.MIN),extrudeamount=1):
+    with BuildPart() as TextMesh:
+        with BuildSketch() as ns:
+            Text(textstring,font="Copperplate Gothic Bold", font_size=font_size, align=align)
+        extrude(amount=extrudeamount)
+    return TextMesh.part
+# %%
+backplate_thickess=3
+text_extrude_amount=2
+borderthickness=2
+# %%
+Name = StringToPart("Joe Neate",font="Copperplate Gothic Bold", font_size=11,align=(Align.CENTER, Align.MIN),extrudeamount=text_extrude_amount)
+Date = StringToPart("November 2023",font="Copperplate Gothic Bold", font_size=6, align=(Align.CENTER, Align.MIN),extrudeamount=text_extrude_amount)
 #%%
+backplate = svgtosketch("/workspaces/Models/SVG/backplate.svg")
+with BuildPart() as plaque:
+    add(backplate)
+    extrude(amount=backplate_thickess)
+# %%
+sk = svgtosketch("/workspaces/Models/SVG/logo.svg")
+with BuildPart() as logo:
+    add(sk)
+    extrude(amount=text_extrude_amount)
+# %%
+bsk = svgtosketch("/workspaces/Models/SVG/border.svg")
+with BuildPart() as border:
+    add(bsk)
+    extrude(amount=text_extrude_amount)
+    topf = border.faces().filter_by(Axis.Z)
+    offset(amount=-1,openings=topf)
+#%%
+plaquebox=plaque.part.bounding_box()
+plaquewidth = plaquebox.size.X
+logobox=logo.part.bounding_box()
+logowidth = logobox.size.X
+centerX = plaquewidth/2
+with BuildPart() as assemble:
+    add(plaque.part)
+    with Locations((0,0,backplate_thickess)):
+        add(border.part)
+    with Locations((centerX-(logowidth/2),24,backplate_thickess)):
+        add(logo.part)
+    with Locations((centerX,15,backplate_thickess)):
+        add(Name)
+    with Locations((centerX,7,backplate_thickess)):
+        add(Date)
+reset_show()
+show_object(assemble)
+# %%
+assemble.part.export_step("plaque.step")
+# %%
+
+# %%
+
+# %%
